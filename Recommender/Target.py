b1bf321ea9dc07c21e27da27d2224c14cd87a147
habijabi <rakibhjoy@gmail.com>
2024-05-08 13:15:05 +0600

    Minor Bug Target Setter Fixed.

--- Recommender/Target.py
+++ Recommender/Target.py
@@ -13,7 +13,7 @@ def get_lo_hi(handle):
     for con in x['result']:
         if 'maxRating' in con:
             current=int(con['maxRating'])
-
+    print("Current Max", current)
     target=0
     if current>=1900:
         current=1900
@@ -24,6 +24,9 @@ def get_lo_hi(handle):
     elif current>=1400:
         current=1400
         target = 1600
+    elif current>=1200:
+        current = 1200
+        target = 1400
     else:
         current=0
         target = 1200
